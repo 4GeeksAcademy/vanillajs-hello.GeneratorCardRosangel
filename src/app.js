6b57f496322b6afcbc9fa6f7d513bcdb6bedf20e
Rosangel593 <148271323+Rosangel593@users.noreply.github.com>
2023-12-07 19:59:50 +0000

    con boton

--- src/app.js
+++ src/app.js
@@ -4,8 +4,37 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+import { Button } from "bootstrap";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  document.querySelector(".center").innerHTML = randomNumbers();
+  let palo = randomIcon();
+  let top = document.querySelector(".top");
+  top.innerHTML = palo;
+  let bottom = document.querySelector(".bottom");
+  bottom.innerHTML = palo;
+
+  function randomNumbers() {
+    let numbers = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+    console.log(numbers);
+
+    return numbers[Math.floor(Math.random() * numbers.length)];
+  }
+
+  function randomIcon() {
+    let icons = ["♦", "♥", "♠", "♣"];
+    console.log(icons);
+    return icons[Math.floor(Math.random() * icons.length)];
+  }
+  if (palo == "♥" || palo == "♦") {
+    console.log(top);
+    top.style.color = "red";
+    bottom.style.color = "red";
+  }
 };
+let botto = document.querySelector("button");
+botto.addEventListener("click", _ => {
+  location.reload();
+});
